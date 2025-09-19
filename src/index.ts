7fb304d27b9b01e217cb91a4c739abec26cc8edb
SeyyedKhandon <seyyedkhandon@gmail.com>
2025-09-19 19:29:24 +0200

    add Blockchain

--- src/index.ts
+++ src/index.ts
@@ -12,10 +12,14 @@ class Block {
   transactions: string[];
   previousHash: string;
   hash: string;
-  constructor(timestamp: string, transactions: string[], previousHash: string) {
+  constructor(
+    timestamp: string,
+    transactions: string[],
+    previousHash: string = ""
+  ) {
     this.timestamp = timestamp;
     this.transactions = transactions;
-    this.previousHash = previousHash;
+    this.previousHash = previousHash; // This should get updated inside the blockchain class through addBlock fn
     this.hash = this.calculateHash();
   }
   calculateHash() {
@@ -24,3 +28,65 @@ class Block {
     ).toString();
   }
 }
+
+/**
+ * A Blockchain has:
+ * - Chain of blocks: Block[]
+ * - Genesis block generator
+ * - Chain verifier
+ * - Add new block
+ * - Get last block
+ */
+class BlockChain {
+  chain: Block[];
+  constructor() {
+    this.chain = [this.generateGenesisBlock()];
+  }
+  generateGenesisBlock() {
+    return new Block("19/09/2025", [], "0");
+  }
+  getLastBlock() {
+    return this.chain.at(-1)!;
+  }
+  addBlock(newBlock: Block) {
+    // Connect the new block to the previous block
+    newBlock.previousHash = this.getLastBlock().hash!;
+    // Update the hash of the new block as it's previousHash got updated
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+      // Check if the block have not been tampered
+      const isTampered = currentBlock.hash !== currentBlock.calculateHash();
+      // Check if block are connected properly to the previous block
+      const isConnectedCorrectly =
+        currentBlock.previousHash === previousBlock.hash;
+      if (isTampered || !isConnectedCorrectly) {
+        console.log("Blockchain is not valid!", {
+          isTampered,
+          isConnectedCorrectly,
+          currentBlock,
+          previousBlock,
+        });
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+const blockchain = new BlockChain();
+console.log("New Blockchain", blockchain);
+
+blockchain.addBlock(new Block("2/2/2025", ["tr1"]));
+blockchain.addBlock(new Block("3/3/2025", ["tr2"]));
+
+console.log("Used Blockchain", blockchain);
+blockchain.isChainValid();
+
+// tamper the blockchain to check the isChainValid
+blockchain.chain[1].transactions = ["tr33"];
+blockchain.isChainValid();
